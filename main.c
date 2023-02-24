9e78851494c74a0b2c23bf03f4160d25d28da5d4
auuauu19697 <122807421+auuauu19697@users.noreply.github.com>
2023-02-24 14:21:26 +0700

    enqueue and dequeue

--- main.c
+++ main.c
@@ -17,15 +17,14 @@ int main(int argc , char **argv) {
    int i,x;
    
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
-        }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
-        }
- }
+  for(i=1;i<argc;i++){
+    if(strcmp(argv[i],"x")==0){
+      x=dequeue(&headPtr,&tailPtr);
+      printf("dequeing %d\n",x);
+    }
+    else {
+      enqueue(&headPtr,&tailPtr, atoi(argv[i]));
+    }
+  }
   return 0;
 }
